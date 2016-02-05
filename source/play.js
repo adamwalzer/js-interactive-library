a62179d90b5a25ab392b86ec5124a08435ed97a2
Micah Rolon <functionmicah@gmail.com>
2016-02-05 15:07:50 -0500

    Resolve event type for interaction events.

--- source/play.js
+++ source/play.js
@@ -15,6 +15,34 @@ import Basic from 'types/Basic';
 import Queue from 'types/Queue';
 import { Point, Size } from 'types/Dimensions';
 
+var pl;
+/**
+ * Resolves the event type for user interactions.
+ */
+function resolveEventType () {
+	var resolution;
+
+	if (game.feature('touch')) {
+		resolution = {
+			ACTION: 'touchend',
+			ACTION_DOWN: 'touchstart',
+			ACTION_UP: 'touchend',
+			ACTION_MOVE: 'touchmove',
+			ACTION_OUT: 'touchcancel'
+		};
+	} else {
+		resolution = {
+			ACTION: 'click',
+			ACTION_DOWN: 'mousedown',
+			ACTION_UP: 'mouseup',
+			ACTION_MOVE: 'mousemove',
+			ACTION_OUT: 'mouseout'
+		};
+	}
+
+	return resolution;
+}
+
 /**
  * Globaly accesable, root level namespace for the library.
  * @namespace
@@ -25,19 +53,20 @@ import { Point, Size } from 'types/Dimensions';
  * @prop {function} game - Registers a game view implementation. Also a namespace for other methods.
  * @prop {object} util - Namespace for utility functions.
  */
-var pl = {
+pl = {
 	Basic, Point, Size, Queue,
 	game,
 	util,
 	
 	/**
 	 * @namespace
-	 * @prop {string} CLICK - The device normalized click event name.
+	 * @prop {string} ACTION - The device normalized `click` event name.
+	 * @prop {string} ACTION_DOWN - The device normalized `mousedown` event name.
+	 * @prop {string} ACTION_UP - The device normalized `mouseup` event name.
+	 * @prop {string} ACTION_MOVE - The device normalized `mousemove` event name.
+	 * @prop {string} ACTION_OUT - The device normalized `mouseout` event name.
 	 */
-	EVENT: {
-		// TODO: Find a better way to test for touch enabled devices.
-		CLICK: (/ipad|iphone|android/i).test(navigator.userAgent) ? 'touchend' : 'click'
-	}
+	EVENT: resolveEventType()
 };
 
 export default pl;
