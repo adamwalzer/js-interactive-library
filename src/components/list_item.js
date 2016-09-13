d50107eff04b6634175014a667613a69454147ca
Adam Walzer <adamwalzer@gmail.com>
2016-09-13 15:46:00 -0400

    list_item updates

--- src/components/list_item.js
+++ src/components/list_item.js
@@ -1,25 +1,18 @@
+import _ from 'lodash';
+
 import Component from './component.js';
 
 class ListItem extends Component {
-  constructor() {
-    super();
-  }
-
   componentWillMount() {
     if (!this.props.correct) {
       this.complete();
     }
   }
-
-  checkComplete() {
-    // list items should not check for being complete
-  }
-
-  render() {
-    return (
-      <li {...this.props} />
-    );
-  }
 }
+
+ListItem.defaultProps = _.defaults({
+  checkComplete: false,
+  type: 'li',
+}, Component.defaultProps);
 
 export default ListItem;
