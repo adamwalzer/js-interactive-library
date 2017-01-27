aa34fd598c0621c9e9faf0af7718c5c8d1ee034b
Adam Walzer <adamwalzer@gmail.com>
2017-01-27 11:19:24 -0500

    selectable reveal improvement

--- src/mixins/selectable_reveal.js
+++ src/mixins/selectable_reveal.js
@@ -3,15 +3,18 @@ export default function (props, opts = {}) {
         <$k.Component className="selectable-reveal">
             <$k.MediaCollection
                 play={_.get(props, 'data.reveal.open')}
+                children={opts.media}
                 {...opts.MediaCollectionProps}
             />
             <$k.Selectable
                 dataTarget="selectable"
+                list={opts.selectables}
                 {...opts.SelectableProps}
             />
             <$k.Reveal
                 openTarget="reveal"
                 openReveal={_.get(props, 'data.selectable.target.props.message')}
+                list={opts.reveals}
                 {...opts.RevealProps}
             />
         </$k.Component>
