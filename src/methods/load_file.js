df8c9230af0de622bf330a1fa36d6a56e216e49e
Adam Walzer <adamwalzer@gmail.com>
2017-01-27 17:32:23 -0500

    load file cache

--- src/methods/load_file.js
+++ src/methods/load_file.js
@@ -1,10 +1,21 @@
 export default function (file, callback) {
-    var xobj = new XMLHttpRequest();
+    let xobj;
+
+    if (!skoash) skoash = {};
+    if (!skoash._cache) skoash._cache = {};
+
+    if (skoash._cache[file]) {
+        callback(skoash._cache[file]);
+        return;
+    }
+
+    xobj = new XMLHttpRequest();
     xobj.overrideMimeType('application/json');
     xobj.open('GET', file, true);
     xobj.onreadystatechange = function () {
         /* eslint-disable eqeqeq */
         if (xobj.readyState == 4 && xobj.status == '200') {
+            skoash._cache[file] = xobj.responseText;
             callback(xobj.responseText);
         }
         /* eslint-enable eqeqeq */
