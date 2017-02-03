3f0b0c8eb5296e2104523af9bc7610fae67235b5
Adam Walzer <adamwalzer@gmail.com>
2017-02-03 17:42:30 -0500

    loadFile update

--- src/methods/load_file.js
+++ src/methods/load_file.js
@@ -1,11 +1,26 @@
 export default function (file, callback) {
-    var xobj = new XMLHttpRequest();
+    let xobj;
+
+    if (!skoash) skoash = {};
+    if (!skoash._cache) skoash._cache = {};
+
+    if (skoash._cache[file]) {
+        callback(skoash._cache[file]);
+        return;
+    }
+
+    xobj = new XMLHttpRequest();
     xobj.overrideMimeType('application/json');
     xobj.open('GET', file, true);
     xobj.onreadystatechange = function () {
         /* eslint-disable eqeqeq */
-        if (xobj.readyState == 4 && xobj.status == '200') {
-            callback(xobj.responseText);
+        if (xobj.readyState == 4) {
+            if (xobj.status == '200') {
+                skoash._cache[file] = xobj.responseText;
+                callback(xobj.responseText);
+            } else {
+                console.log('Error', xobj.statusText); // eslint-disable-line no-console
+            }
         }
         /* eslint-enable eqeqeq */
     };
